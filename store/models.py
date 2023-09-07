2fa0d655dc41be5b87aa7e3194ba6a22589cff0f
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-09-06 19:59:07 -0700

    Added cart functionality

--- store/models.py
+++ store/models.py
@@ -38,12 +38,29 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        order_items = self.orderitems_set.all()
+        cart_total = sum(item.get_total for item in order_items)
+        return cart_total
+    
+    @property
+    def get_cart_items(self):
+        order_items = self.orderitems_set.all()
+        quantity_total = sum(item.quantity for item in order_items)
+        return quantity_total
 
 class OrderItems(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     data_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
